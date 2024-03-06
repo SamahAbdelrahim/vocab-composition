1022e4004fa8a6844ff0ec95fb17572fa1e4a910
SamahAbdelrahim <samah_osman@outlook.com>
2024-03-06 09:44:35 +0000

    add server file

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3002;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'experiment')));
+
+// Set up a route for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'experiment', 'solidity-ratings.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
