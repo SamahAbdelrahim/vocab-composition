69fddf190d939d72ba96714ab488cc4d47d23318
SamahAbdelrahim <samah_osman@outlook.com>
2024-10-29 21:25:22 -0700

    clean code + Kappa

--- helper.R
+++ helper.R
@@ -26,3 +26,36 @@ map_categories <- function(category, block) {
     return(NA)  # Handle cases where block is not recognized
   }
 }
+
+
+
+find_shared_words1 <- function(df1, df2, prop1, prop2) {
+  inner_join(
+    df1 %>% filter(proportion <= ifelse(prop1 == "low", 0.5, Inf)),
+    df2 %>% filter(proportion <= ifelse(prop2 == "low", 0.5, Inf)),
+    by = "theword"
+  ) %>% 
+    select(theword) %>%
+    unique()
+}
+
+find_shared_words <- function(df1, df2, name1, name2, prop1, prop2) {
+  # Filter based on proportion condition (<= 0.5 for "low", > 0.5 for "high")
+  filtered_df1 <- df1 %>% filter(proportion <= ifelse(prop1 == "low", 0.5, Inf))
+  filtered_df2 <- df2 %>% filter(proportion <= ifelse(prop2 == "low", 0.5, Inf))
+  
+  # Perform inner join to get shared words
+  shared_words <- inner_join(filtered_df1, filtered_df2, by = "theword") %>%
+    select(theword) %>%
+    unique()
+  
+  # Add metadata
+  shared_words %>%
+    mutate(
+      source1 = name1,
+      source2 = name2,
+      condition1 = prop1,
+      condition2 = prop2
+    )
+}
+
